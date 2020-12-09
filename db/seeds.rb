b856ecde7b65b7d910aacf78bf05f88b682d3e5c
Chip Jackson <chip.jackson@faithlife.com>
2020-12-09 06:05:23 -0800

    Add db:seed script.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+me = User.find_by(uid: "2456610")
+
+unless me then raise "Strava uid not found." end
+
+me.activities = %x(cat db/activities.json | jq -cr '.').chomp
+me.save!
+puts "Success!"

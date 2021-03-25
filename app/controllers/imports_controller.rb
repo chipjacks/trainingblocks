cb3e0bbd2db0fdbeefb22b24e0636f8c2d769852
Chip Jackson <chipjacks@gmail.com>
2021-03-25 14:10:26 -0700

    Handle delete webhook event.

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -8,9 +8,14 @@ class ImportsController < ApplicationController
   def strava_push
     user = User.find_by!(provider: Import::STRAVA, uid: params[:owner_id])
     if (params[:object_type] == "activity")
-      import = Import.find_or_create(params[:object_id], Import::STRAVA, {}, user)
-      import.stale = true
-      import.save!
+      if (params[:aspect_type] == "delete")
+        import = Import.find(params[:object_id])
+        import.destroy!
+      else # "create" or "update"
+        import = Import.find_or_create(params[:object_id], Import::STRAVA, {}, user)
+        import.stale = true
+        import.save!
+      end
     else
       # TODO: handle api deauthorization
     end

afa0fb25bcf5489fe6f969d258331335827400be
Chip Jackson <chipjacks@gmail.com>
2021-03-27 13:07:31 -0700

    Remove dead strava session code.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,11 +7,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       if @user.previously_new_record?
         InitialStravaImportJob.perform_now(@user, credentials["token"])
       end
-      sign_in_and_redirect @user, event: :authentication
-      session["devise.strava_access_token"] = credentials["token"]
-      session["devise.strava_refresh_token"] = credentials["refresh_token"]
-      session["devise.strava_expires_at"] = credentials["expires_at"]
       set_flash_message(:notice, :success, kind: "Strava") if is_navigational_format?
+      store_location_for(:user, :root_path)
+      sign_in_and_redirect @user, event: :authentication
     else
       set_flash_message(:alert, :failure, kind: "Strava", reason: "#{@user.errors.full_messages.join(', ')}")
       redirect_to root_path

8578a406c5c4a2f75448c0da2fe8969994454fc9
Chip Jackson <chipjacks@gmail.com>
2021-07-15 14:17:05 -0700

    Add rollbar client access token env variable.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def elm_flags(flags)
     flags = flags || {}
-    flags[:rollbar_access_token] = Rollbar.configuration.access_token || ""
+    flags[:rollbar_access_token] = Rails.configuration.rollbar_client_access_token
     flags[:environment] = Rails.env
     tag.meta(id: 'elm-flags', data: {flags: flags.to_json})
   end
--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module TrainingBlocks
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.rollbar_client_access_token = ENV['ROLLBAR_CLIENT_ACCESS_TOKEN']
   end
 end

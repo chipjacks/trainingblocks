380a5e42c21dc208aed1be4ad4866c736f8c8d57
Chip Jackson <chipjacks@gmail.com>
2021-07-15 15:27:00 -0700

    Add user_id flag in helper function.

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,6 +2,5 @@ class CalendarController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @elm_flags = { user_id: current_user.id }
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
-  def elm_flags(flags)
-    flags = flags || {}
+  def elm_flags
+    flags = {}
+    flags[:user_id] = current_user.id
     flags[:rollbar_access_token] = Rails.configuration.rollbar_client_access_token
     flags[:environment] = Rails.env
     tag.meta(id: 'elm-flags', data: {flags: flags.to_json})

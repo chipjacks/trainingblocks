dbb8c0a8b02f8a86600e375e56bf5971ca2859c0
Chip Jackson <chipjacks@gmail.com>
2021-07-20 06:53:24 -0700

    Fix dev server live reload.

--- config/webpack/loaders/elm.js
+++ config/webpack/loaders/elm.js
@@ -1,22 +1,24 @@
-const { resolve } = require('path')
+const { resolve } = require("path");
 
-const isProduction = process.env.NODE_ENV === 'production'
-const elmSource = resolve(process.cwd())
-const elmMake = `${elmSource}/node_modules/.bin/elm`
+const isProduction = process.env.NODE_ENV === "production";
+const elmSource = resolve(process.cwd());
+const elmMake = `${elmSource}/node_modules/.bin/elm`;
 
-const elmDefaultOptions = { cwd: elmSource, pathToElm: elmMake }
+const elmDefaultOptions = { cwd: elmSource, pathToElm: elmMake };
 const developmentOptions = Object.assign({}, elmDefaultOptions, {
   verbose: true,
   debug: false,
-})
+});
 
 const elmWebpackLoader = {
-  loader: 'elm-webpack-loader',
-  options: isProduction ? elmDefaultOptions : developmentOptions
-}
+  loader: "elm-webpack-loader",
+  options: isProduction ? elmDefaultOptions : developmentOptions,
+};
 
 module.exports = {
   test: /\.elm(\.erb)?$/,
   exclude: [/elm-stuff/, /node_modules/],
-  use: isProduction ? [elmWebpackLoader] : [{ loader: 'elm-hot-webpack-loader' }, elmWebpackLoader]
-}
+  use: isProduction
+    ? [elmWebpackLoader]
+    : [{ loader: "elm-hot-webpack-loader" }, elmWebpackLoader],
+};

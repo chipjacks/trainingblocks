84e2fdf54489b26ef0d12d169615b971be718e30
Chip Jackson <chipjacks@gmail.com>
2021-07-30 15:21:56 -0700

    Open lap form automatically.

--- app/javascript/ActivityForm.elm
+++ app/javascript/ActivityForm.elm
@@ -39,14 +39,14 @@ init activity =
     let
         ( editing, completion, laps ) =
             case ( activity.laps, activity.planned ) of
-                ( [ lap ], [] ) ->
-                    ( False, Completed, Selection.init [ lap ] )
+                ( [ lap ], _ ) ->
+                    ( True, Completed, Selection.init [ lap ] )
 
                 ( lap :: more, _ ) ->
                     ( False, Completed, Selection.init (lap :: more) )
 
-                ( [], [ lap ] ) ->
-                    ( False, Planned, Selection.init [ lap ] )
+                ( _, [ lap ] ) ->
+                    ( True, Planned, Selection.init [ lap ] )
 
                 ( _, lap :: more ) ->
                     ( False, Planned, Selection.init (lap :: more) )

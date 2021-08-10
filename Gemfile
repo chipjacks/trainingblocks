9701bcb241c16cd5e82c720f3fa81b43a0386f7d
Chip Jackson <chipjacks@gmail.com>
2021-08-10 16:17:07 -0700

    Use letter_opener in development to send emails to browser.

--- Gemfile
+++ Gemfile
@@ -58,6 +58,7 @@ end
 
 group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
+  gem 'letter_opener', '~> 1.7'
 end
 
 group :test do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,11 +35,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {
-    api_key: ENV['MAILGUN_API_KEY'],
-    domain: 'mg.rhinolog.app',
-  }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log

c2e1fc4a508178f6bc4f8bf0de92c228ff10bb5b
Chip Jackson <chipjacks@gmail.com>
2021-08-10 11:23:15 -0700

    Fix action mailer error.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,13 +20,15 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.to_i}",
     }
   else
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
   end
+
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false

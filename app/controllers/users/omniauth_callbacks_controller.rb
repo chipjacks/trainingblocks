75a0a38776f770dcea4482678a85b2d94326966f
Chip Jackson <chipjacks@gmail.com>
2021-08-23 15:45:09 -0700

    Make sure saved users have an email.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -17,16 +17,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         )
       end
       redirect_to account_path
+    elsif !@user.email
+      session[:oauth_user] = @user
+      flash[:notice] = 'Please enter an email for your account.'
+      redirect_to new_user_registration_path
     elsif @user.persisted?
       store_location_for(@user, calendar_path)
       if is_navigational_format?
         set_flash_message(:notice, :success, kind: 'Strava')
       end
       sign_in_and_redirect @user, event: :authentication
-    elsif !@user.email
-      session[:oauth_user] = @user
-      flash[:notice] = 'Please enter an email for your account.'
-      redirect_to new_user_registration_path
     else
       set_flash_message(
         :alert,

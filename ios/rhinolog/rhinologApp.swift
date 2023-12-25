a747547c1fa029a659e40f42eafda61e772e8a9e
Chip Jackson <chipjacks@gmail.com>
2023-12-25 07:03:07 -0800

    init ios app

---
+++ ios/rhinolog/rhinologApp.swift
@@ -0,0 +1,17 @@
+//
+//  rhinologApp.swift
+//  rhinolog
+//
+//  Created by Chip Jackson on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct rhinologApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

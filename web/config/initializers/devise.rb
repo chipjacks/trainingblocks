3b01b9311349151c4a9b2f9b04511b307837bec5
Chip Jackson <chipjacks@gmail.com>
2023-12-31 08:12:28 -0800

    point config

--- web/config/initializers/devise.rb
+++ web/config/initializers/devise.rb
@@ -282,6 +282,6 @@ Devise.setup do |config|
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+    jwt.secret = Rails.application.secrets.devise_jwt_secret_key!
   end
 end
